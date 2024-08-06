5b3141695dd43c791be46194637cadba6c4f274f
prathyusha <chinthakuntaprathyusha2001@gmail.com>
2024-08-06 19:39:29 +0530

    add:homepage

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -7,100 +7,102 @@ import {
   Grid,
 } from '@mui/material';
 import {
-  FastForward as FastForwardIcon,
-  MonetizationOn as MonetizationOnIcon,
-  DesignServices as DesignServicesIcon,
-  AddCircle as AddCircleIcon,
-  Refresh as RefreshIcon,
-  Cancel as CancelIcon,
-} from '@mui/icons-material';
+  FaForward,
+  FaMoneyBill,
+  FaPaintBrush,
+  FaPlusCircle,
+  FaSync,
+  FaTimesCircle,
+} from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 const Homepage = () => {
   const tests = [
     {
       title: 'Fast Delivery',
       description: 'Receive your design within two business days on average',
-      icon: <FastForwardIcon />,
+      icon: <FaForward />,
     },
     {
-      title: ' Fixed monthly cost',
+      title: 'Fixed monthly cost',
       description: 'No surprises, pay the same fixed price each month.',
-      icon: <MonetizationOnIcon />,
+      icon: <FaMoneyBill />,
     },
     {
       title: 'Custom made designs',
       description:
         'Each of your designs is made especially for you and is 100% yours.',
-      icon: <DesignServicesIcon />,
+      icon: <FaPaintBrush />,
     },
     {
       title: 'Unlimited Designs',
       description:
         'Add as many design requests as you want. Delivered one by one.',
-      icon: <AddCircleIcon />,
+      icon: <FaPlusCircle />,
     },
     {
       title: 'Unlimited Changes',
       description:
         "We'll continue to revise the design until you're 100% satisfied.",
-      icon: <RefreshIcon />,
+      icon: <FaSync />,
     },
     {
       title: 'Cancel or pause anytime',
       description:
         'Scale up or down as needed, and pause or cancel at anytime.',
-      icon: <CancelIcon />,
+      icon: <FaTimesCircle />,
     },
   ];
-
   return (
-    <Container>
-      <Box textAlign='center' mt={8} mb={5}>
-        <Typography variant='h3' component='h2' gutterBottom>
-          Great design, without the overhead.
-        </Typography>
-        <Typography variant='h6' sx={{ maxWidth: '840px', mx: 'auto' }}>
-          Our team has over a decade of experience building consumer and
-          enterprise solutions for two-sided marketplaces and Software as a
-          Service companies.
-        </Typography>
-      </Box>
-      <Grid container spacing={4}>
-        {tests.map((test, index) => (
-          <Grid item xs={12} sm={6} md={6} key={index}>
-            <Card
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                height: '160px',
-                bgcolor: '#f5f5f5',
-              }}
-            >
-              <CardContent sx={{ flex: '1 0 auto' }}>
-                <Box
-                  sx={{
-                    fontSize: 20,
-                    bgcolor: '#FFCCCC',
-                    color: '#FF0014',
-                    p: 1,
-                    display: 'inline-flex',
-                    borderRadius: '4px',
-                  }}
-                >
-                  {test.icon}
-                </Box>
-                <Typography variant='h5' sx={{ mt: 3 }}>
-                  {test.title}
-                </Typography>
-                <Typography variant='body1' color='textSecondary'>
-                  {test.description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <Box sx={{ bgcolor: '#ffffff', py: 8 }}>
+      <Container>
+        <Box textAlign='center' mb={5}>
+          <Typography variant='h3' component='h2' gutterBottom>
+            Great design, without the overhead.
+          </Typography>
+          <Typography variant='h6' sx={{ maxWidth: '840px', mx: 'auto' }}>
+            Our team has over a decade of experience building consumer and
+            enterprise solutions for two-sided marketplaces and Software as a
+            Service companies.
+          </Typography>
+        </Box>
+        <Grid container spacing={4}>
+          {tests.map((test, index) => (
+            <Grid item xs={12} sm={6} md={6} key={index}>
+              <Card
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: '160px',
+                  bgcolor: '#f5f5f5',
+                }}
+              >
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Box
+                    sx={{
+                      fontSize: 20,
+                      bgcolor: '#FFCCCC',
+                      color: '#FF0014',
+                      p: 1,
+                      display: 'inline-flex',
+                      borderRadius: '4px',
+                    }}
+                  >
+                    {test.icon}
+                  </Box>
+                  <Typography variant='h5' sx={{ mt: 3 }}>
+                    {test.title}
+                  </Typography>
+                  <Typography variant='body1' color='textSecondary'>
+                    {test.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
--- app/components/Homepage1.tsx
+++ app/components/Homepage1.tsx
@@ -59,98 +59,96 @@ const Homepage1 = () => {
 
   const titleColors = ['#FF0027', '#7EFF2A', '#3357FF', '#A300A3'];
   return (
-    <Box sx={{ bgcolor: '#f5f5f5' }}>
-      <Container>
-        <Box py={5} width={'72%'}>
-          <Typography variant='h4'>
-            Designs that increase signups, conversion and retention
-          </Typography>
-          <Typography variant='body1' color='textSecondary'>
-            Our team has over a decade of experience building consumer and
-            enterprise solutions for two-sided marketplaces and Software as a
-            Service companies.
-          </Typography>
-        </Box>
-        <Grid container spacing={4}>
-          {projects.map((project, index) => (
-            <Grid item xs={12} sm={6} md={6} key={index}>
-              <Card
+    <Container sx={{ py: 8 }}>
+      <Box py={5} width={'72%'}>
+        <Typography variant='h4'>
+          Designs that increase signups, conversion and retention
+        </Typography>
+        <Typography variant='body1' color='textSecondary'>
+          Our team has over a decade of experience building consumer and
+          enterprise solutions for two-sided marketplaces and Software as a
+          Service companies.
+        </Typography>
+      </Box>
+      <Grid container spacing={4}>
+        {projects.map((project, index) => (
+          <Grid item xs={12} sm={6} md={6} key={index}>
+            <Card
+              sx={{
+                display: 'flex',
+                height: 320,
+                backgroundImage: `url(${project.imgSrc})`,
+                backgroundSize: 'cover',
+              }}
+            >
+              <Box
                 sx={{
                   display: 'flex',
+                  flexDirection: 'column',
                   height: 320,
-                  backgroundImage: `url(${project.imgSrc})`,
-                  backgroundSize: 'cover',
                 }}
               >
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 320,
-                  }}
-                >
-                  <CardContent sx={{ flex: '1 0 auto' }}>
-                    <Typography
-                      variant='h5'
-                      sx={{
-                        mb: 2,
-                        color: titleColors[index % titleColors.length],
-                      }}
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography
+                    variant='h5'
+                    sx={{
+                      mb: 2,
+                      color: titleColors[index % titleColors.length],
+                    }}
+                  >
+                    {project.title}
+                  </Typography>
+                  <Typography
+                    variant='body2'
+                    color='textSecondary'
+                    sx={{ mb: 2, maxWidth: '240px' }}
+                  >
+                    {project.description}
+                  </Typography>
+                </CardContent>
+                <Box sx={{ p: 2 }} maxWidth={400}>
+                  {project.tags.map((tag, i) => (
+                    <Button
+                      size='small'
+                      key={i}
+                      sx={{ mb: 1, mr: 1, bgcolor: '#f5f5f5' }}
                     >
-                      {project.title}
-                    </Typography>
-                    <Typography
-                      variant='body2'
-                      color='textSecondary'
-                      sx={{ mb: 2, maxWidth: '240px' }}
-                    >
-                      {project.description}
-                    </Typography>
-                  </CardContent>
-                  <Box sx={{ p: 2 }} maxWidth={400}>
-                    {project.tags.map((tag, i) => (
-                      <Button
-                        size='small'
-                        key={i}
-                        sx={{ mb: 1, mr: 1, bgcolor: '#f5f5f5' }}
-                      >
-                        {tag}
-                      </Button>
-                    ))}
-                  </Box>
+                      {tag}
+                    </Button>
+                  ))}
                 </Box>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-        <Box sx={{ py: 5, width: 1 }} textAlign='center'>
-          <Typography
-            variant='h3'
-            sx={{ maxWidth: '700px', mx: 'auto' }}
-            gutterBottom
-          >
-            Design, Development, Automation & More
-          </Typography>
-          <Box>
-            {tags.map((tag, index) => (
-              <Box
-                key={index}
-                sx={{
-                  display: 'inline-block',
-                  px: 2,
-                  py: 1,
-                  border: '1px solid black',
-                  borderRadius: 4,
-                  m: 1,
-                }}
-              >
-                {tag}
               </Box>
-            ))}
-          </Box>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      <Box sx={{ py: 5, width: 1 }} textAlign='center'>
+        <Typography
+          variant='h3'
+          sx={{ maxWidth: '700px', mx: 'auto' }}
+          gutterBottom
+        >
+          Design, Development, Automation & More
+        </Typography>
+        <Box>
+          {tags.map((tag, index) => (
+            <Box
+              key={index}
+              sx={{
+                display: 'inline-block',
+                px: 2,
+                py: 1,
+                border: '1px solid black',
+                borderRadius: 4,
+                m: 1,
+              }}
+            >
+              {tag}
+            </Box>
+          ))}
         </Box>
-      </Container>
-    </Box>
+      </Box>
+    </Container>
   );
 };
 
