5e52cc2e9ad039216c944c4a28c65d3c0bd80f7d
prathyusha <chinthakuntaprathyusha2001@gmail.com>
2024-08-08 21:17:29 +0530

    add:design4

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Design1 from './components/Design1';
 import Design2 from './components/Design2';
 import Design3 from './components/Design3';
+import Design4 from './components/Design4';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Design1 />
       <Design2 />
       <Design3 />
+      <Design4 />
     </div>
   );
 }
